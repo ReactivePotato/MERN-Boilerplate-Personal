be37562878a1a6bfc8fb95b75597bbb762b3f4bf
ReactivePotato <potamatto@gmail.com>
2022-01-30 23:55:56 +1000

    added react router and routes

--- client/config/root.jsx
+++ client/config/root.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const Root = () => {
 	return <div>
-		<h1>This is Root Component</h1>
+		<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<div>This is React Router v6</div>} />
+			<Route path="/test" element={<div>This is Test Page</div>} />
+			<Route path="*" element={<div><h3>404 Not Found</h3></div>} />
+		</Routes>
+		</BrowserRouter>
 	</div>
 }
 
